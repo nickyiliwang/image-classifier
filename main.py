a00d0f1d593155c512f5993b336b55ae930e6483
nickyiliwang <nickyiliwang@gmail.com>
2022-09-16 01:48:18 -0400

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+from transformers import ViTFeatureExtractor, ViTForImageClassification
+from PIL import Image
+import requests
+
+url = 'https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_960_720.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+
+feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+
+inputs = feature_extractor(images=image, return_tensors="pt")
+outputs = model(**inputs)
+logits = outputs.logits
+# model predicts one of the 1000 ImageNet classes
+predicted_class_idx = logits.argmax(-1).item()
+print("Predicted class:", model.config.id2label[predicted_class_idx])
